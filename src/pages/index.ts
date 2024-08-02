2a324ce199d8470ba4594d726a242ff0ac83d10a
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2024-08-02 16:21:36 +0200

    Fix: import

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,3 @@
 export { default as Home } from './home/page';
 export { default as Login } from './login/page';
-export { default as Auth } from './auth/page';
 export { default as Register } from './register/page';
