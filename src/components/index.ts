8c8fb33920cd66429f57c6dc563084eab9c2ac3c
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2024-08-02 16:18:00 +0200

    Feat: ProtectedRoute component

---
+++ src/components/index.ts
@@ -0,0 +1,14 @@
+// Application
+export { PageLayout } from './PageLayout';
+export { Header } from './Header';
+export { ProtectedRoute } from './ProtectedRoute';
+
+// Components
+export { Modal } from './Modal';
+
+// Dropzone
+export { Dropzone } from './Dropzone';
+export { ImageDropzone } from './ImageDropzone';
+
+// Form
+export * from './Form';
